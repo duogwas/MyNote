4bfbfaa59d83c481ee0206955df2324f00b80de1
Dương Hoàng <duong2001n@gmail.com>
2024-06-05 15:22:13 +0700

    note detail screen

---
+++ app/src/main/java/com/duogwas/mynote/Model/Note.java
@@ -0,0 +1,49 @@
+package com.duogwas.mynote.Model;
+
+public class Note {
+    public Long id;
+    public String title;
+    public String content;
+    public int pinned;
+    public String created_at;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getPinned() {
+        return pinned;
+    }
+
+    public void setPinned(int pinned) {
+        this.pinned = pinned;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+}
